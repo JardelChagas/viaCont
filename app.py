69d9214fc14deb6c632c4f6cc48076c3132a0df9
Jardel <jardelchags@gmail.com>
2022-05-08 15:47:26 -0300

    ajuste nome

--- app.py
+++ app.py
@@ -2,25 +2,24 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():  # put application's code here
+def hello_world():
     return render_template("index.html", title="ViaConte")
 
 @app.route('/quemsomos')
-def quemSomos():  # put application's code here
+def quemSomos():
     return render_template("quemSomos.html", title="Quem somos")
 
-@app.route('/servic os')
-def servicos():  # put application's code here
+@app.route('/servicos')
+def servicos():
     return render_template("servicos.html", title="Serviços")
 
 @app.route('/blog')
-def blog():  # put application's code here
+def blog():
     return render_template("blog.html", title="Blog")
 
 @app.route('/entreEmContato')
-def entreEmContato():  # put application's code here
+def entreEmContato():
     return render_template("entreEmContato.html", title="Entre em contato")
 
 if __name__ == '__main__':
